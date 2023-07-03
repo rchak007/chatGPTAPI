8128f04ea23018b51bf19aa0c9346923cd0d2645
ChakravartiRaghavan <rchak1@aol.com>
2023-07-02 23:22:03 -0700

    1stTest

---
+++ chatgptapi/src/app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const API_KEY = process.env.OpenAiKey;
+
+async function fetchData() {
+    const response = await fetch("https://api.openai.com/v1/completions", {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "text-davinci-003",
+            prompt: "how many planets are there in solar system?",
+            max_tokens: 7
+        })
+    })
+    const data = await response.json()
+    console.log(data)
+}
+
+fetchData()
